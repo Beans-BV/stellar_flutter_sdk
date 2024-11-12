9cb0679b3a0b25d037d47d506c82eee7b396c5c1
Cuong Pham <cuong.pham@sea-solutions.com>
2024-11-12 18:33:05 +0700

    Hotswap: Add option to sponsor transaction

--- lib/src/sep/0099/hotswap.dart
+++ lib/src/sep/0099/hotswap.dart
@@ -69,30 +69,42 @@ class Hotswap {
     required String accountId,
     required HotswapRoute hotswapRoute,
     String toAssetTrustLineLimit = ChangeTrustOperationBuilder.MAX_LIMIT,
+    String? sponsoringAccountId,
   }) async {
     var account = await _sdk.accounts.account(accountId);
 
-    final txBuilder = TransactionBuilder(account);
-
-    final toAsset = hotswapRoute.toAsset.toAsset();
-    final trustDestinationAssetOperation = ChangeTrustOperationBuilder(
-      toAsset,
-      toAssetTrustLineLimit,
-    ).build();
-
-    final hotswapHandlerAccountId = hotswapRoute.toAddress;
-
+    final hotswapHandlerAccountId = hotswapRoute.hotswapAddress;
     final fromAsset = hotswapRoute.fromAsset.toAsset();
     final fromAssetBalanceObject = account.balances.firstWhere(
       (e) =>
           e.assetCode == fromAsset.code && e.assetIssuer == fromAsset.issuerId,
     );
+    final toAsset = hotswapRoute.toAsset.toAsset();
+    final sponsored = sponsoringAccountId != null;
+
+    final txBuilder = TransactionBuilder(account);
+
+    if (sponsored) {
+      BeginSponsoringFutureReservesOperation beginSponsoringOperation =
+          BeginSponsoringFutureReservesOperationBuilder(accountId)
+              .setSourceAccount(sponsoringAccountId)
+              .build();
+      txBuilder.addOperation(beginSponsoringOperation);
+    }
+
+    final trustDestinationAssetOperation = ChangeTrustOperationBuilder(
+      toAsset,
+      toAssetTrustLineLimit,
+    ).build();
+    txBuilder.addOperation(trustDestinationAssetOperation);
+
     // Send from asset to the hotswap server
     final depositSourceAssetOperation = PaymentOperationBuilder(
       hotswapHandlerAccountId,
       fromAsset,
       fromAssetBalanceObject.balance,
     ).build();
+    txBuilder.addOperation(depositSourceAssetOperation);
 
     // Receive to asset from hotswap server
     final receiveDestinationAssetOperation = PaymentOperationBuilder(
@@ -100,17 +112,21 @@ class Hotswap {
       toAsset,
       fromAssetBalanceObject.balance, // Ensuring a 1:1 exchange
     ).setSourceAccount(hotswapHandlerAccountId).build();
+    txBuilder.addOperation(receiveDestinationAssetOperation);
 
     final untrustSourceAssetOperation = ChangeTrustOperationBuilder(
       fromAsset,
       '0',
     ).build();
+    txBuilder.addOperation(untrustSourceAssetOperation);
 
-    txBuilder
-        .addOperation(trustDestinationAssetOperation)
-        .addOperation(depositSourceAssetOperation)
-        .addOperation(receiveDestinationAssetOperation)
-        .addOperation(untrustSourceAssetOperation);
+    if (sponsored) {
+      EndSponsoringFutureReservesOperation endSponsorshipOperation =
+          EndSponsoringFutureReservesOperationBuilder()
+              .setSourceAccount(accountId)
+              .build();
+      txBuilder.addOperation(endSponsorshipOperation);
+    }
 
     final transaction = txBuilder.build();
 
@@ -180,28 +196,23 @@ class Hotswap {
   }
 }
 
-class HotswapRoute extends Response {
-  final String toAddress;
+class HotswapRoute {
+  final String hotswapAddress;
   final String fromAsset;
   final String toAsset;
-  final double minimumAmount;
 
-  HotswapRoute({
-    required this.toAddress,
+  const HotswapRoute({
+    required this.hotswapAddress,
     required this.fromAsset,
     required this.toAsset,
-    required this.minimumAmount,
   });
 
   factory HotswapRoute.fromJson(Map<String, dynamic> json) {
     return HotswapRoute(
-      toAddress:
+      hotswapAddress:
           (json['hotswap_address'] ?? json['receivables_address']) as String,
       fromAsset: (json['from_asset'] ?? json['you_send_asset']) as String,
       toAsset: (json['to_asset'] ?? json['we_send_asset']) as String,
-      minimumAmount: double.parse(
-        (json['min_amount'] ?? json['minimum_amount']) as String,
-      ),
     );
   }
 }
@@ -209,7 +220,9 @@ class HotswapRoute extends Response {
 class _HotswapInfoResponse {
   final List<HotswapRoute> hotswapRoutes;
 
-  _HotswapInfoResponse({required this.hotswapRoutes});
+  const _HotswapInfoResponse({
+    required this.hotswapRoutes,
+  });
 
   factory _HotswapInfoResponse.fromJson(Map<String, dynamic> json) {
     if (json.containsKey('hotswap_routes')) {
